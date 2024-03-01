6e4c0ed9d745010aa763f3e012dc36aead9ad852
Biparnak Roy <roybiparnak@gmail.com>
2024-03-01 10:56:32 +0530

    roll back

--- Fatigue_Detection_App/admin_views.py
+++ Fatigue_Detection_App/admin_views.py
@@ -56,6 +56,42 @@ class View_worker(APIView):
         if request.user.user_type == '1':
             worker = Worker.objects.get(worker_uuid=worker_uuid)
             worker_custom_user = CustomUser.objects.get(id=worker.user_id)
+            
+            # fatigue_levels = ["No Fatigue", "Low Fatigue", "Medium Fatigue"]
+            # rand_idx = random.randint(1, len(fatigue_levels)-1)
+            # current_fatigue = fatigue_levels[rand_idx]
+            # print(worker_custom_user.last_login)
+        
+            # print(type(worker_custom_user.last_login))
+            # print(datetime.now().replace(tzinfo=tz('Asia/Kolkata'))-worker_custom_user.last_login)
+            # #dateA = dateA.replace(tzinfo=tz('UTC'))
+            # if worker.last_fatigue_state == None:
+            #     worker.last_fatigue_state = worker_custom_user.last_login
+            #     worker.save()
+                
+            # diff=datetime.now().replace(tzinfo=tz('Asia/Kolkata'))-worker.last_fatigue_state
+            
+            # if diff.total_seconds()/60 > 15.0: 
+            #     fatigue_state= Fatigue_State.objects.create(fatigue_state_worker=worker,fatigue_state=current_fatigue)
+            #     worker.last_fatigue_state=datetime.now().replace(tzinfo=tz('Asia/Kolkata'))
+            #     worker.save()
+
+            # # sending mail to admin
+            # if current_fatigue == "Medium Fatigue" or current_fatigue == "High Fatigue"
+            #     subject = f'{current_fatigue} Detected for {worker_custom_user.first_name} {worker_custom_user.last_name}'
+            #     message = f'Dear Admin, {current_fatigue} Detected for {worker_custom_user.first_name} {worker_custom_user.last_name} at {worker.last_fatigue_state}.'
+            #     email_from = settings.EMAIL_HOST_USER
+            #     recipient_list = ["	parijatgd@gmail.com","roybiparnak@gmail.com" ]
+            #     send_mail( subject, message, email_from, recipient_list )
+            #     # sending mail to admin
+            #     if current_fatigue == "Medium Fatigue" or current_fatigue == "High Fatigue":
+            #         subject = f'{current_fatigue} Detected for {worker_custom_user.first_name} {worker_custom_user.last_name}'
+            #         message = f'Dear Admin, {current_fatigue} Detected for {worker_custom_user.first_name} {worker_custom_user.last_name} at {worker.last_fatigue_state.replace(tzinfo=tz("Asia/Kolkata"))}.'
+            #         email_from = settings.EMAIL_HOST_USER
+            #         recipient_list = ["	parijatgd@gmail.com","roybiparnak@gmail.com" ]
+            #         send_mail( subject, message, email_from, recipient_list )
+
+
 
             fatigue_states = Fatigue_State.objects.filter(fatigue_state_worker=worker).order_by('-fatigue_state_created_at')
             context = {
